2af0a2944511a365b851374de754ef55b2f50850
Lachlan McCalman <lachlan@mccalman.info>
2026-02-14 06:33:34 +1100

    remove plans folder

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ from pathlib import Path
 project_root = Path(__file__).parent
 output_dir = project_root / "output"
 templates_dir = project_root / "templates"
-alpine_src = project_root / "alpine3.15.8.min.js"
-pico_src = project_root / "pico.jade.min.css"
+assets_dir = project_root / "assets"
+alpine_src = assets_dir / "alpine3.15.8.min.js"
+pico_src = assets_dir / "pico.jade.min.css"
 form_dir = project_root / "form"
 
 # ---------------------------------------------------------------------------
